8e55387757bc9a267acc1ef263cc40e7c49fccfa
SOLA <93892724+ffe4el@users.noreply.github.com>
2022-05-14 15:18:45 +0900

    Update Caesar_gui.py

--- HW17/Caesar_gui.py
+++ HW17/Caesar_gui.py
@@ -1,21 +1,23 @@
-def caesar_encode(text: str, shift: int) -> str:
+import tkinter as tk
+
+def caesar_encode(master, shift=3) -> str:
     ascii_list = []
-    for letter in text:
+    for letter in master:
         ascii_list.append(ord(letter))
     ch_ascii_list = []
     for i in range(len(ascii_list)):
         if 65 <= ascii_list[i] <= 87:
-            ch_ascii_list.append(chr(ascii_list[i]+shift))
+            ch_ascii_list.append(chr(ascii_list[i] + shift))
         elif 88 <= ascii_list[i] <= 90:
-            ch_ascii_list.append(chr(ascii_list[i]-20-shift))
+            ch_ascii_list.append(chr(ascii_list[i] - 20 - shift))
         elif 97 <= ascii_list[i] <= 119:
-            ch_ascii_list.append(chr(ascii_list[i]+shift))
+            ch_ascii_list.append(chr(ascii_list[i] + shift))
         elif 120 <= ascii_list[i] <= 122:
-            ch_ascii_list.append(chr(ascii_list[i]-20-shift))
+            ch_ascii_list.append(chr(ascii_list[i] - 20 - shift))
 
-    return ch_ascii_list
+    return "".join(ch_ascii_list)
 
-def caesar_decode(text: str, shift: int) -> str:
+def caesar_decode(text: str, shift=3) -> str:
     ascii_list = []
     for letter in text:
         ascii_list.append(ord(letter))
@@ -32,11 +34,37 @@ def caesar_decode(text: str, shift: int) -> str:
     return ch_ascii_list1
 
 def main():
-    text = str(input("암호를 입력하시오.=> "))
-    string = caesar_encode(text, 3)
-    print("result_encoding =>","".join(string))
-    string = caesar_decode(text, 3)
-    print("result_decoding =>","".join(string))
+    window = tk.Tk()
+    window.title("caesar_code")
+    window.resizable(width=False, height=False)  # false면 크기 조정 불가!
+    frm_entry = tk.Frame(master=window)
+    ent_code = tk.Entry(master=frm_entry, width=10)
+
+    lbl_code = tk.Label(master=frm_entry, text="caesar_code")
+    ent_code.grid(row=0, column=0, sticky="e")
+    lbl_code.grid(row=0, column=1, sticky="w")
+    btn_convert = tk.Button(
+        master=window,
+        text="\N{RIGHTWARDS BLACK ARROW}",
+    )
+    lbl_result = tk.Label(master=window, text="encoding")
+    frm_entry.grid(row=0, column=0, padx=10)
+    lbl_result.grid(row=0, column=2, padx=10)
+
+    btn_convert = tk.Button(
+        master=window,
+        text="\N{RIGHTWARDS BLACK ARROW}",
+        command=caesar_encode
+    )
+    btn_convert.grid(row=0, column=1, pady=10)
+    window.mainloop()
+
+    # text = str(input("암호를 입력하시오.=> "))
+    # string = caesar_encode(text, 3)
+    # print("result_encoding =>","".join(string))
+
+
+
 
 if __name__ == "__main__":
     main()
